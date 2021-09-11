bb4a685e79831968441246170d3680fe6f12af1f
samurainisa <aressamurai@mail.ru>
2021-09-11 22:54:39 +0700

    Добавьте файлы проекта.

---
+++ SadovikovPi202_laba0/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace SadovikovPi202_laba0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Катет A";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(125, 343);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(120, 23);
+            this.textBox5.TabIndex = 5;
+
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Катет B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Высота H";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Масса M";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 346);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Плотность";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Вывод";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(146, 45);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 15);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Задание 1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(534, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 15);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Задание 2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 26);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Генерация CSV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(396, 143);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(366, 161);
+            this.dataGridView1.TabIndex = 22;
+
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(512, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 23;
+
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(377, 96);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(133, 15);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Количество абонентов";
+
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(525, 322);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 45);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Заполнить таблицу";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(125, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 26;
+
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(125, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 27;
+
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(125, 168);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 28;
+
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(125, 203);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 23);
+            this.textBox6.TabIndex = 29;
+
+            this.textBox6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 462);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Main form";
+
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ SadovikovPi202_laba0/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Data;
+using System.Collections.Generic;
+
+namespace SadovikovPi202_laba0
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            double a, b, h, m, P;
+
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show("Введите катет А");
+            }
+            else if (textBox3.Text == "")
+            {
+                MessageBox.Show("Введите катет В");
+            }
+            else if (textBox4.Text == "")
+            {
+                MessageBox.Show("Введите высоту H");
+            }
+            else if (textBox6.Text == "")
+            {
+                MessageBox.Show("Введите массу M");
+            }
+            else
+            {
+                a = Convert.ToDouble(textBox2.Text);
+                b = Convert.ToDouble(textBox3.Text);
+                h = Convert.ToDouble(textBox4.Text);
+                m = Convert.ToDouble(textBox6.Text);
+                P = 2 * m / (a * b * h);
+                textBox5.Text = P.ToString();
+
+                double[] mass = new double[] { a, b, h, m };
+
+                if (mass.Contains(0))
+                    MessageBox.Show("Значения не могут быть равны 0");
+
+            }
+        }
+
+
+
+        /*Телефонный справочник содержит данные об абонентах: фамилия, номер телефона (число формата xxyy),
+         *годе установки телефон (формата аабб). вывести данные об абонентах, у которых уу=бб
+         *а) сгенерировать данные, сохраняя их в формате csv, xml, или json
+         *б) считывать данные из файлы и выполнять операции в соответствии с заданием*/
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Введите число в поле");
+            }
+            else if (textBox1.Text.Contains((char)0))
+            {
+                MessageBox.Show("Количество не может быть 0");
+            }
+            else
+            {
+
+
+                var folderpath = @"C:\Sadovikov_lab0";
+
+                if (!Directory.Exists(folderpath))
+                {
+                    Directory.CreateDirectory(folderpath);
+                }
+
+                Random rnd = new Random();
+                var filepath = @"C:\Sadovikov_lab0\csvtable.csv";
+
+                using (StreamWriter writer = new StreamWriter(new FileStream(filepath, FileMode.Create, FileAccess.Write), Encoding.Default))
+                {
+
+                    for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+                    {
+                        writer.WriteLine(String.Format("{0},{1},{2}", RandomGeneratorName(5, true), rnd.Next(1000, 9999), rnd.Next(1000, 9999)));
+                    }
+                }
+            }
+        }
+
+
+        public string RandomGeneratorName(int length, bool name)
+        {
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            char[] chars = new char[length];
+            string validCharsLower = "abcdefghijklmnopqrstuvwxyz";
+            string validCharsHigher = "ABCEDFGHIJKLMNOPQRSTUVWXYZ";
+            for (int i = 0; i < length; i++)
+            {
+                if (i == 0 || !name)
+                {
+                    byte[] bytes = new byte[1];
+                    rng.GetBytes(bytes);
+                    Random rnd = new Random(bytes[0]);
+                    chars[i] = validCharsHigher[rnd.Next(0, validCharsHigher.Length - 1)];
+                }
+                else
+                {
+                    byte[] bytes = new byte[1];
+                    rng.GetBytes(bytes);
+                    Random rnd = new Random(bytes[0]);
+                    chars[i] = validCharsLower[rnd.Next(0, validCharsLower.Length - 1)];
+                }
+
+            }
+            return (new string(chars));
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LoadInfo();
+        }
+        private void LoadInfo()
+        {
+            string delimiter = ",";
+            string tableName = "abonents";
+            string fileName = ("C:\\Sadovikov_lab0\\csvtable.csv");
+
+            DataSet dataset = new DataSet();
+            StreamReader sr = new StreamReader(fileName);
+
+            dataset.Tables.Add(tableName);
+            dataset.Tables[tableName].Columns.Add("Surname");
+            dataset.Tables[tableName].Columns.Add("Number");
+            dataset.Tables[tableName].Columns.Add("Date");
+
+            string allData = sr.ReadToEnd();
+            string[] rows = allData.Split("\r".ToCharArray());
+
+            foreach(string r in rows)
+            {
+                string value = r.Trim('\n');
+                if (value.Trim() == String.Empty)
+                    continue;
+                string[] items = r.Split(delimiter.ToCharArray());
+                string yy = items[1].Substring(2, 2);
+                string bb = items[2].Substring(2, 2);
+                if (yy == bb)
+     
+                    dataset.Tables[tableName].Rows.Add(items);
+
+            }
+            this.dataGridView1.DataSource = dataset.Tables[0].DefaultView;
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+          }
+       }
+
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+    }
+ }
+                    
+                
+            
+
+           
